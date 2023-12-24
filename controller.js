0163d6f3ba956ccb31349cc2303a0d7ce9495ed1
Alexander <saneknv05@gmail.com>
2023-12-24 15:44:40 +0300

    fix

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ const db = require('./db.js');
 
 class Controller {
     async get(req, res){
-        const items = await db.query(`SELECT id, name, title, price, img FROM cooldatabase`);
+        const items = await db.query(`SELECT id, name, title, price, img FROM units`);
         res.json(items.rows);
         console.log('get items', items.rows[0]);        
     }
