33711a5829287acf96b9b23607d15e82c575a936
Alexander <saneknv05@gmail.com>
2023-12-24 15:30:09 +0300

    a

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ const db = require('./db.js');
 
 class Controller {
     async get(req, res){
-        const items = await db.query(`SELECT id, name, title, price, img FROM cars`);
+        const items = await db.query(`SELECT id, name, title, price, img FROM cooldatabase`);
         res.json(items.rows);
         console.log('get items', items.rows[0]);        
     }
